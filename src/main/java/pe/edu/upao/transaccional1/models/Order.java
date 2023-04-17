2d4cd58fcaffa85db6908327a3effc11be8d58ea
Darkwolfreded <estefano_27_07@hotmail.com>
2023-04-17 00:21:32 -0500

    Creación clase Order

---
+++ src/main/java/pe/edu/upao/transaccional1/models/Order.java
@@ -0,0 +1,83 @@
+package pe.edu.upao.transaccional1.models;
+
+import java.util.List;
+
+public class Order {
+
+    int id;
+    String customerName;
+    String customerEmail;
+    String status;
+    String creationTime;
+    String estimatedDeliveryTime;
+    List<Food> items;
+
+    public Order() {
+    }
+
+    public Order(int id, String customerName, String customerEmail, String status, String creationTime, String estimatedDeliveryTime, List<Food> items) {
+        this.id = id;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.status = status;
+        this.creationTime = creationTime;
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+        this.items = items;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getEstimatedDeliveryTime() {
+        return estimatedDeliveryTime;
+    }
+
+    public void setEstimatedDeliveryTime(String estimatedDeliveryTime) {
+        this.estimatedDeliveryTime = estimatedDeliveryTime;
+    }
+
+    public List<Food> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Food> items) {
+        this.items = items;
+    }
+}
